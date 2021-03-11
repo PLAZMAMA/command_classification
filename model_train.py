d30be23e8cbb4993cc276bf564e4ff0ff11d7764
PLAZMAMA <maorioriori@gmail.com>
2021-03-11 18:03:43 -0500

    changed some of the preprocessing to account for the new class of data

--- model_train.py
+++ model_train.py
@@ -1,36 +1,9 @@
-import tensorflow as tf
+from Model import Model
 import numpy as np
-import tensorflow_hub as hub
-import tensorflow_text as text
 import pandas as pd
 from sklearn.utils import shuffle
 
-class Model:
-    preprocessor = hub.KerasLayer("http://tfhub.dev/tensorflow/albert_en_preprocess/3")
-
-    def __init__(self):
-        text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-        encoder_inputs = self.preprocessor(text_input)
-        encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/albert_en_base/2")
-        outputs = encoder(encoder_inputs)
-        net = tf.keras.layers.Dense(256)(outputs['pooled_output'])
-        net = tf.keras.layers.Dropout(0.1)(net)
-        net = tf.keras.layers.Dense(2, activation='softmax', dtype=tf.float32)(net)
-        self.model = tf.keras.Model(text_input, net)
-
-    def train(self, train_data, train_labels, epochs=5):
-        """trains the model using the data given"""
-        encoder_inputs = self.preprocessor(train_data)
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.model.fit(train_data, train_labels, batch_size=16, epochs=epochs)
-
-    def evaluate(self, test_data, test_labels):
-        """evaluates the model given the test data and labels"""
-        results = self.model.evaluate(test_data, test_labels, batch_size=16)
-        print(f'Total Loss: {results[0]}, Total Accuracy: {results[1] * 100}%')
-
 if __name__ == '__main__':
-
     #creating instance of the model class to be used later and a train_portion for later use 
     model = Model()
     train_portion = 0.9 #how much of the data will go into training(0-1)
@@ -38,10 +11,11 @@ if __name__ == '__main__':
     #fetching the data from the csv files
     notes_data = pd.read_csv('notes_data.csv')['notes commands'].to_numpy(dtype=np.str)
     calories_data = pd.read_csv('calories_data.csv', delimiter=',')['calories commands'].to_numpy(dtype=np.str)
+    smp_data = pd.read_csv('smp.csv')['commands'].to_numpy(dtype=np.str)
 
     #labeling and combining the data together
-    data = np.array(notes_data.tolist() + calories_data.tolist(), dtype=np.str)
-    labels = np.array([[1, 0]] * notes_data.shape[0] + [[1, 0]] * calories_data.shape[0])
+    data = np.array(notes_data.tolist() + calories_data.tolist() + smp_data.tolist(), dtype=np.str)
+    labels = np.array([[1, 0, 0]] * notes_data.shape[0] + [[0, 1, 0]] * calories_data.shape[0] + [[0, 0, 1]] * smp_data.shape[0])
 
     #mixing/shuffling the data the data
     shuffled_data, shuffled_labels = shuffle(data, labels)
