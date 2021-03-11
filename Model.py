1c5077cb48b2e2fc6451ba1e0983de4c59f8be92
PLAZMAMA <maorioriori@gmail.com>
2021-03-11 17:28:48 -0500

    changed the dense layer to 3 nodes since I added another class, added new predict method and some instance variables

--- Model.py
+++ Model.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
+import numpy as np
 
 class Model:
     preprocessor = hub.KerasLayer("http://tfhub.dev/tensorflow/albert_en_preprocess/3")
+    classes = ['notes command', 'calories command', 'random']
+    trained = False
 
     def __init__(self):
         text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
@@ -12,7 +15,7 @@ class Model:
         outputs = encoder(encoder_inputs)
         net = tf.keras.layers.Dense(256)(outputs['pooled_output'])
         net = tf.keras.layers.Dropout(0.3)(net)
-        net = tf.keras.layers.Dense(2, activation='softmax', dtype=tf.float32)(net)
+        net = tf.keras.layers.Dense(3, activation='softmax', dtype=tf.float32)(net)
         self.model = tf.keras.Model(text_input, net)
 
     def train(self, train_data, train_labels, epochs=5):
@@ -20,8 +23,23 @@ class Model:
         encoder_inputs = self.preprocessor(train_data)
         self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         self.model.fit(train_data, train_labels, batch_size=16, epochs=epochs)
+        trained = True
 
     def evaluate(self, test_data, test_labels):
         """evaluates the model given the test data and labels"""
         results = self.model.evaluate(test_data, test_labels, batch_size=16)
         print(f'Total Loss: {results[0]}, Total Accuracy: {results[1] * 100}%')
+    
+    def predict(self, prediction_data, model_num=None):
+        """uses the model to predict/classify which class the prediction data falls into"""
+        if model_num:
+            model = tf.keras.models.load_model(f'/code/models/bert_{model_num}')
+            result = model.predict(prediction_data)
+
+        elif trained:
+            result = self.model.predict(prediction_data)
+
+        else:
+            raise RuntimeError('"model num" argument was not passed and this instance of the model class hasnt run the train function')
+
+        return self.classes[np.argmax(result)]
